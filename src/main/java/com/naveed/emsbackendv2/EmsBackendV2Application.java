df0a0cc82c5f209722f534a496046e205f1d936e
Naveed <raysnaveed02@gmail.com>
2026-03-09 00:35:18 +0700

    feat: initial spring boot project structure
    - Stack: Spring Boot 3.5.11 (GA) and Java 21 (LTS)
    - Lombok: Eliminate boilerplate (getters/setters) for cleaner entities
    - Web: Essential for implementing RESTful API endpoints
    - JPA & Postgres: Persistence layer for the 5-table relational design
    - Validation: Ensure data integrity on all incoming API requests
    - Security & OAuth2: Foundation for Feature 5 (Keycloak/Token auth)
    - DevTools: Enhance development workflow with automatic restarts

---
+++ src/main/java/com/naveed/emsbackendv2/EmsBackendV2Application.java
@@ -0,0 +1,13 @@
+package com.naveed.emsbackendv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsBackendV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/naveed/emsbackendv2/EmsBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveed.emsbackendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
